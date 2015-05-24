76a3318092a2882c4ad2297ee004a490b1dc455b
Skyler <skymanzanares@hotmail.com>
2015-05-24 09:44:15 -0600

    Added missing import statement

--- src/nmt/minecraft/QuestManager/QuestManagerPlugin.java
+++ src/nmt/minecraft/QuestManager/QuestManagerPlugin.java
@@ -2,6 +2,8 @@ package nmt.minecraft.QuestManager;
 
 import java.util.List;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Provided API and Command Line interaction between enlisted quest managers and
  * the user. <br />
