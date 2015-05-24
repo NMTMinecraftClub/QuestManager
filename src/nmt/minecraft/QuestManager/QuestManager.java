9732bf7e442bc4223b105cb24a72aa963953a38d
Skyler <skymanzanares@hotmail.com>
2015-05-24 09:20:57 -0600

    Began laying out structural classes

---
+++ src/nmt/minecraft/QuestManager/QuestManager.java
@@ -0,0 +1,5 @@
+package nmt.minecraft.QuestManager;
+
+public class QuestManager {
+
+}
---
+++ src/nmt/minecraft/QuestManager/QuestManagerPlugin.java
@@ -0,0 +1,18 @@
+package nmt.minecraft.QuestManager;
+
+import java.util.List;
+
+/**
+ * Provided API and Command Line interaction between enlisted quest managers and
+ * the user. <br />
+ * 
+ * @author Skyler
+ *
+ */
+public class QuestManagerPlugin extends JavaPlugin {
+	
+	public static QuestManagerPlugin questManagerPlugin;
+	
+	private List<QuestManager> managers;
+	
+}
