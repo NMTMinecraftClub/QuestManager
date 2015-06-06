3024df431983b43748f28b8d349fb03119843eb3
Skyler <skymanzanares@hotmail.com>
2015-06-06 17:03:50 -0600

    Updated history to allow for saving to config

--- src/nmt/minecraft/QuestManager/Quest/History/History.java
+++ src/nmt/minecraft/QuestManager/Quest/History/History.java
@@ -1,11 +1,35 @@
 package nmt.minecraft.QuestManager.Quest.History;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 public class History {
 	
 	private List<HistoryEvent> events;
+	
+	@SuppressWarnings("unchecked")
+	public static History fromConfig(YamlConfiguration config) throws InvalidConfigurationException {
+		if (config == null) {
+			return null;
+		}
+		if (!config.contains("HistoryEvents")) {
+			throw new InvalidConfigurationException();
+		}
+		
+		History history = new History();
+		
+		List<HistoryEvent> list;
+		
+		list = (List<HistoryEvent>) config.get("HistoryEvents");
+		
+		history.events = list;
+		
+		return history;
+	}
 	
 	/**
 	 * Creates a new history with no events
@@ -42,6 +66,28 @@ public class History {
 		builder = builder.substring(0, builder.length() - 3);
 		
 		return builder;
+	}
+	
+	/**
+	 * Returns a config file (section) that stores the information contained in this history
+	 * @return
+	 */
+	public YamlConfiguration toConfig() {
+		
+		YamlConfiguration config = new YamlConfiguration();
+		
+		if (events.isEmpty()) {
+			return config;
+		}
+		List<String> list = new ArrayList<String>(events.size());
+		
+		for (HistoryEvent e : events) {
+			list.add(e.getDescription());
+		}
+		
+		config.set("HistoryEvents", list);
+		
+		return config;
 	}
 	
 }
