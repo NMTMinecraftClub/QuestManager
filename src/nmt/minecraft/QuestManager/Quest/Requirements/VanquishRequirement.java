8ccbc3ee06f24d83d047d8dd972d62cf9741a5c2
Skyler <skymanzanares@hotmail.com>
2015-06-07 16:26:08 -0600

    Updated to go with State Save and Requirement

--- src/nmt/minecraft/QuestManager/Quest/Requirements/VanquishRequirement.java
+++ src/nmt/minecraft/QuestManager/Quest/Requirements/VanquishRequirement.java
@@ -1,15 +1,24 @@
 package nmt.minecraft.QuestManager.Quest.Requirements;
 
 import nmt.minecraft.QuestManager.QuestManagerPlugin;
+import nmt.minecraft.QuestManager.Configuration.RequirementState;
+import nmt.minecraft.QuestManager.Configuration.StatekeepingRequirement;
 import nmt.minecraft.QuestManager.Quest.Goal;
 import nmt.minecraft.QuestManager.Quest.Requirement;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Requirement that a given entity must be slain.<br />
@@ -18,7 +27,7 @@ import org.bukkit.event.entity.EntityDeathEvent;
  * @author Skyler
  *
  */
-public class VanquishRequirement extends Requirement implements Listener {
+public class VanquishRequirement extends Requirement implements Listener, StatekeepingRequirement {
 	
 	private LivingEntity foe;
 	
@@ -71,6 +80,66 @@ public class VanquishRequirement extends Requirement implements Listener {
 		}
 		
 		state = !foe.isDead();
+	}
+
+	@Override
+	public RequirementState getState() {
+		RequirementState myState = new RequirementState();
+		
+		myState.set("type", "vr");
+		
+		ConfigurationSection foeSection = myState.createSection("foe");
+		foeSection.set("type", foe.getType().name());
+		foeSection.set("maxhp", foe.getMaxHealth());
+		foeSection.set("hp", foe.getHealth());
+		foeSection.set("name", foe.getCustomName());
+		foeSection.set("location", foe.getLocation());
+		
+		//do equips the dirty way :S
+		ConfigurationSection equips = foeSection.createSection("equipment");
+		EntityEquipment equipment = foe.getEquipment();
+		
+		equips.set("head", equipment.getHelmet());
+		equips.set("chest", equipment.getChestplate());
+		equips.set("legs", equipment.getLeggings());
+		equips.set("boots", equipment.getBoots());
+		equips.set("held", equipment.getItemInHand());
+		
+		return myState;
+	}
+
+	@Override
+	public void loadState(RequirementState myState) throws InvalidConfigurationException {
+		
+		if (!myState.contains("type") || !myState.getString("type").equals("vr")) {
+			throw new InvalidConfigurationException();
+		}
+		
+		ConfigurationSection foeState = myState.getConfigurationSection("foe");
+		Location loc = (Location) foeState.get("location");
+		
+		foe = (LivingEntity) loc.getWorld().spawnEntity(loc, EntityType.valueOf(foeState.getString("type")));
+		foe.setMaxHealth(foeState.getDouble("maxhp"));
+		foe.setHealth(foeState.getDouble("hp"));
+		foe.setCustomName(foeState.getString("name"));
+		
+		EntityEquipment equipment = foe.getEquipment();
+		equipment.setHelmet(
+				(ItemStack) foeState.get("head"));
+		equipment.setChestplate(
+				(ItemStack) foeState.get("chest"));
+		equipment.setLeggings(
+				(ItemStack) foeState.get("legs"));
+		equipment.setBoots(
+				(ItemStack) foeState.get("boots"));
+		equipment.setItemInHand(
+				(ItemStack) foeState.get("held"));
+	}
+
+	@Override
+	public void fromConfig(YamlConfiguration config) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
