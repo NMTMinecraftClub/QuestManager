9848c9a21ac4859fbb4b4f354f979d4f04013bf1
Skyler <skymanzanares@hotmail.com>
2015-06-07 18:24:13 -0600

    Updated Goal to have load from config/state methods!

--- src/nmt/minecraft/QuestManager/Quest/Goal.java
+++ src/nmt/minecraft/QuestManager/Quest/Goal.java
@@ -3,6 +3,13 @@ package nmt.minecraft.QuestManager.Quest;
 import java.util.LinkedList;
 import java.util.List;
 
+import nmt.minecraft.QuestManager.QuestManagerPlugin;
+import nmt.minecraft.QuestManager.Configuration.GoalState;
+import nmt.minecraft.QuestManager.Configuration.RequirementState;
+import nmt.minecraft.QuestManager.Configuration.StatekeepingRequirement;
+import nmt.minecraft.QuestManager.Quest.Requirements.RequirementType;
+
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 /**
@@ -21,8 +28,52 @@ public class Goal {
 	
 	private Quest quest;
 	
-	
-	public static Goal fromConfig(YamlConfiguration config) {
+	/**
+	 * Creates a goal from the provided goal configuration
+	 * @param config
+	 * @return
+	 * @throws InvalidConfigurationException 
+	 */
+	public static Goal fromConfig(Quest quest, YamlConfiguration config) throws InvalidConfigurationException {
+		/* goal construction configuration involves:
+		 * Goal name, description
+		 * The requirements that are in it
+		 * 
+		 * The req's are in a list, with each element being a con section with the
+		 * key being the type of req and the value being the config section for setting
+		 * up the req
+		 */
+		
+		if (!config.contains("type") || !config.getString("type").equals("goalcnf")) {
+			throw new InvalidConfigurationException();
+		}
+		
+		String name, description;
+		
+		name = config.getString("name", "");
+		description = config.getString("description", "");
+		
+
+		Goal goal = new Goal(quest, name, description);
+		
+		@SuppressWarnings("unchecked")
+		List<YamlConfiguration> reqs = (List<YamlConfiguration>) config.getList("requirements");
+		if (reqs == null || reqs.isEmpty()) {
+			return goal;
+		}
+		
+		for (YamlConfiguration req : reqs) {
+			String type = req.getKeys(false).iterator().next();
+			YamlConfiguration conf = (YamlConfiguration) req.getConfigurationSection(type);
+			
+			Requirement r = RequirementType.valueOf(type).instance();
+			
+			r.fromConfig(conf);
+			
+			goal.addRequirement(r);
+		}
+		
+		return goal;
 		
 	}
 	
@@ -36,6 +87,37 @@ public class Goal {
 	
 	public Goal(Quest quest, String name) {
 		this(quest, name, "");
+	}
+	
+	public void loadState(GoalState state) throws InvalidConfigurationException {
+		
+		if (!state.getName().equals(name)) {
+			QuestManagerPlugin.questManagerPlugin.getLogger().warning("Loading state information"
+					+ "from a file that has a mismatched goal name!");
+		}
+		
+		//WARNING:
+		//this is assuming that the lists are maintianed in the right order.
+		//it should work this way, but this is a point of error!
+		for (RequirementState rs : state.getRequirementStates()) {
+			if (rs instanceof StatekeepingRequirement) {
+				((StatekeepingRequirement) rs).loadState(rs);
+			}
+		}
+	}
+	
+	public GoalState getState() {
+		
+		GoalState state = new GoalState();
+		state.setName(name);
+		
+		for (Requirement req : requirements) {
+			if (req instanceof StatekeepingRequirement) {
+				state.addRequirementState(((StatekeepingRequirement) req).getState());
+			}
+		}
+		
+		return state;
 	}
 
 	/**
@@ -86,16 +168,6 @@ public class Goal {
 		
 		return true;
 	}
-	
-	
-	public YamlConfiguration toConfig() {
 		
-		YamlConfiguration config = new YamlConfiguration();
-		
-		//TODO
-		
-		return config;
-	}
-	
 	
 }
