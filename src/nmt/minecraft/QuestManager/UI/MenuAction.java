ed6e5fd0b98a9b4a12f24a5edb7e89866e8d1646
Skyler <skymanzanares@hotmail.com>
2015-06-14 15:23:16 -0600

    Added an interface for registering actions to menus

---
+++ src/nmt/minecraft/QuestManager/UI/MenuAction.java
@@ -0,0 +1,12 @@
+package nmt.minecraft.QuestManager.UI;
+
+/**
+ * Specifies an action for the menu to perform on click.
+ * @author Skyler
+ *
+ */
+public interface MenuAction {
+	
+	public void onAction();
+	
+}
