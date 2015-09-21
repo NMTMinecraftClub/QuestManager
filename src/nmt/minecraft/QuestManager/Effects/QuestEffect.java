f59eeeff6941895d6389241978309c9b3aabc375
Skyler Manzanares <skymanzanares@hotmail.com>
2015-09-20 20:23:44 -0600

    Added backbone for an effect system

---
+++ src/nmt/minecraft/QuestManager/Effects/QuestEffect.java
@@ -0,0 +1,15 @@
+package nmt.minecraft.QuestManager.Effects;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+/**
+ * An effect used by the QuestManager to signal something to the player.
+ * @author Skyler
+ *
+ */
+public abstract class QuestEffect {
+
+	public abstract void play(Player player, Location location);
+	
+}
