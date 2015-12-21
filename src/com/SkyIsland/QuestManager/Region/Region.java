92cc29f0549e80228b50f3439b4c362ca8379f10
Skyler Manzanares <skymanzanares@hotmail.com>
2015-12-21 13:28:05 -0700

    Spiffed up regions to give more info

--- src/com/SkyIsland/QuestManager/Region/Region.java
+++ src/com/SkyIsland/QuestManager/Region/Region.java
@@ -10,9 +10,26 @@ import org.bukkit.entity.Entity;
  */
 public abstract class Region {
 	
+	/**
+	 * Checks whether the provided entity is in the region
+	 * @param e
+	 * @return
+	 */
 	public abstract boolean isIn(Entity e);
 	
+	/**
+	 * Checks whether a specified location falls within this region
+	 * @param loc
+	 * @return
+	 */
 	public abstract boolean isIn(Location loc);
+	
+	/**
+	 * Returns a random location from within this region.
+	 * @param safe Whether or not the location should be safe to spawn a normal mob (2x1)
+	 * @return
+	 */
+	public abstract Location randomLocation(boolean safe);
 	
 	@Override
 	public abstract int hashCode();
--- src/com/SkyIsland/QuestManager/Region/SphericalRegion.java
+++ src/com/SkyIsland/QuestManager/Region/SphericalRegion.java
@@ -1,39 +1,73 @@
 package com.SkyIsland.QuestManager.Region;
 
+import java.util.Random;
+
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.util.Vector;
 
 public class SphericalRegion extends Region {
 	
+	private World world;
+	
 	private Vector center;
 	
 	private double radius;
 	
-	public SphericalRegion(Vector center, double radius) {
+	public SphericalRegion(World world, Vector center, double radius) {
+		this.world = world;
 		this.center = center;
 		this.radius = radius;
 	}
 
 	@Override
 	public boolean isIn(Entity e) {
-		return e.getLocation().toVector().distance(center) <= radius;
+		return e.getLocation().getWorld().equals(world) && 
+				e.getLocation().toVector().distance(center) <= radius;
 	}
 
 	@Override
 	public boolean isIn(Location loc) {
-		return loc.toVector().distance(center) <= radius;
+		return loc.getWorld().equals(world) &&
+				loc.toVector().distance(center) <= radius;
 	}
 
 	@Override
 	public int hashCode() {
 		return (int) (112000 
+				+ (800 * world.getName().hashCode())
 				+ (7 * center.getY())
 				+ (17 * center.getZ())
 				+ (23 * center.getX())
 				+ (13 * radius));
 	}
 	
+	@Override
+	public Location randomLocation(boolean safe) {
+		Location loc = world.getSpawnLocation().clone();
+		loc.setX(center.getX());
+		loc.setY(center.getY());
+		loc.setZ(center.getZ());
+		
+		Random rand = new Random();
+		double rad = rand.nextDouble() * radius;
+		double dir = rand.nextDouble() * 360;
+		
+		loc.add(rad * Math.cos(dir), 0, rad * Math.sin(dir));
+		
+		if (!safe) {
+			return loc;
+		}
+		
+		//check y, climb up till we are out of our y
+		if (loc.getBlock().getType().isSolid() ||
+				loc.clone().add(0,1,0).getBlock().getType().isSolid()) {
+			return randomLocation(safe);
+		}
+		
+		return loc;
+	}
 	
 	
 }
