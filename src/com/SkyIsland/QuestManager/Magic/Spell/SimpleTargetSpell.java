4f83cbc4b03e183b71be0a910b0b0c7b4e0695b4
Skyler Manzanares <skymanzanares@hotmail.com>
2015-12-22 11:48:17 -0700

    Updated starget to configuration serializable

--- src/com/SkyIsland/QuestManager/Magic/Spell/SimpleTargetSpell.java
+++ src/com/SkyIsland/QuestManager/Magic/Spell/SimpleTargetSpell.java
@@ -1,15 +1,123 @@
 package com.SkyIsland.QuestManager.Magic.Spell;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Sound;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.util.Vector;
 
+import com.SkyIsland.QuestManager.QuestManagerPlugin;
 import com.SkyIsland.QuestManager.Magic.MagicUser;
 import com.SkyIsland.QuestManager.Magic.Spell.Effect.SpellEffect;
 
 public class SimpleTargetSpell extends TargetSpell {
+	
+	/**
+	 * Registers this class as configuration serializable with all defined 
+	 * {@link aliases aliases}
+	 */
+	public static void registerWithAliases() {
+		for (aliases alias : aliases.values()) {
+			ConfigurationSerialization.registerClass(SimpleTargetSpell.class, alias.getAlias());
+		}
+	}
+	
+	/**
+	 * Registers this class as configuration serializable with only the default alias
+	 */
+	public static void registerWithoutAliases() {
+		ConfigurationSerialization.registerClass(SimpleTargetSpell.class);
+	}
+	
+
+	private enum aliases {
+		DEFAULT(SimpleTargetSpell.class.getName()),
+		LONG("SimpleTargetSpell"),
+		SHORT("STargetSpell");
+		
+		private String alias;
+		
+		private aliases(String alias) {
+			this.alias = alias;
+		}
+		
+		public String getAlias() {
+			return alias;
+		}
+	}
+
+	public static SimpleTargetSpell valueOf(Map<String, Object> map) {
+		if (!map.containsKey("cost") || !map.containsKey("name") || !map.containsKey("description")
+				|| !map.containsKey("speed") || !map.containsKey("maxdistance") 
+				|| !map.containsKey("effects")) {
+			QuestManagerPlugin.questManagerPlugin.getLogger().warning(
+					"Unable to load spell " 
+						+ (map.containsKey("name") ? (String) map.get("name") : "")
+						+ ": Missing some keys!"
+					);
+			return null;
+		}
+		
+		SimpleTargetSpell spell = new SimpleTargetSpell(
+				(int) map.get("cost"),
+				(String) map.get("name"),
+				(String) map.get("description"),
+				(double) map.get("speed"),
+				(int) map.get("maxdistance")
+				);
+		
+		@SuppressWarnings("unchecked")
+		List<SpellEffect> effects = (List<SpellEffect>) map.get("effects");
+		for (SpellEffect effect : effects) {
+			spell.addSpellEffect(effect);
+		}
+		
+		if (map.containsKey("projectileeffect")) {
+			spell.setProjectileEffect(Effect.valueOf((String) map.get("projectileeffect")));
+		}
+		if (map.containsKey("contacteffect")) {
+			spell.setContactEffect(Effect.valueOf((String) map.get("contacteffect")));
+		}
+		if (map.containsKey("castsound")) {
+			spell.setCastSound(Sound.valueOf((String) map.get("castsound")));
+		}
+		if (map.containsKey("contactsound")) {
+			spell.setContactSound(Sound.valueOf((String) map.get("contactsound")));
+		}
+		
+		return spell;
+	}
+	
+	public Map<String, Object> serialize() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		map.put("cost", getCost());
+		map.put("name", getName());
+		map.put("description", getDescription());
+		map.put("speed", speed);
+		map.put("maxdistance", maxDistance);
+		
+		map.put("effects", getSpellEffects());
+		
+		if (contactEffect != null) {
+			map.put("contacteffect", contactEffect.name());
+		}
+		if (castSound != null) {
+			map.put("castsound", castSound.name());
+		}if (projectileEffect != null) {
+			map.put("projectileeffect", projectileEffect.name());
+		}
+		if (contactSound != null) {
+			map.put("contactsound", contactSound.name());
+		}
+		
+		return map;
+	}
 	
 	private double speed;
 	
